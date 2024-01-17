128dae00f23ac950ca3a3f39d45d66f82011371d
Ybz1234 <jonathanbz49@gmail.com>
2024-01-17 20:30:03 +0200

    Initial commit

---
+++ scripts/functions.js
@@ -0,0 +1,40 @@
+function Start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(Update, 10)
+        isRunning = true;
+    }
+}
+
+function Stop(){
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+
+function Reset(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+}
+
+function Update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let miliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds  = String(seconds).padStart(2, "0");
+    miliseconds = String(miliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+}
---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function Main(){
+    
+}
+
+Main();
